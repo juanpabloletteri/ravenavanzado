f9fd7ad9b02373fa4c414afd010e66c0f12b3d76
juanpabloletteri <pabloletteri@gmail.com>
2023-11-17 15:29:28 -0300

    esqueleto con algunas imagenes y cambio

--- src/app/paginas/p1/p1.page.ts
+++ src/app/paginas/p1/p1.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class P1Page implements OnInit {
 
+  contador: number = 1;
+  puntaje: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  elegirOpcion(opcion: number) {
+    this.contador += 1;
   }
 
 }
