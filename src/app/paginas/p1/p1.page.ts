80796d136b25ebe9c716f70363e61068bf5e9d72
juanpabloletteri <pabloletteri@gmail.com>
2023-11-17 15:42:46 -0300

    leer json respuestas

--- src/app/paginas/p1/p1.page.ts
+++ src/app/paginas/p1/p1.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-p1',
@@ -9,10 +11,25 @@ export class P1Page implements OnInit {
 
   contador: number = 1;
   puntaje: number = 0;
+  respuestas: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getRespuestas().subscribe(res => {
+      this.respuestas = res;
+      console.log("respuetas", this.respuestas);
+    });
+  }
+
+  getRespuestas() {
+    return this.http
+      .get("assets/respuestas.json")
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      )
   }
 
   elegirOpcion(opcion: number) {
